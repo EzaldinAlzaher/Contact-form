5124f5664e332d072b132811965f7331d8d7ae21
EzaldinAlzaher <ezaldinalzaher@gmail.com>
2024-11-12 21:52:43 +0300

    Update on Contact.jsx and Media.jsx files

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -4,21 +4,21 @@ export const Contact = () => {
     <>
 
     <div className="text-white leading-10 my-16 xl:text-[20px]">    
-        <div className="flex items-center gap-2 ">
+        <div className="flex items-center gap-2 hv-c">
             <img src="/public/Images/Vector.png" alt="" className="w-6 h-[18px]"/>
             <span>
                 <a href="mailto" className="no-underline text-white">ezaldinalzaher@gmail.com</a>
             </span>
         </div>
 
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 my-3 hv-c">
             <img src="/public/Images/tel.png" alt="" className="w-6 h-6"/>
             <span>
                 <a href="tel" className="no-underline text-white">+1 723 456 789</a>
             </span>
         </div>
 
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 hv-c">
             <img src="/public/Images/loc.png" alt="" className="w-6 h-7"/>
             <span>123 Street 456 House</span>
         </div>
--- src/Components/Media.jsx
+++ src/Components/Media.jsx
@@ -2,15 +2,15 @@ export const Media = () => {
   return (
     <>
       <div className="media flex justify-center items-center mt-16 mb-8">
-        <a href="" className="mx-6 bg-dark_pink w-16 h-16 rounded-[50%]">
+        <a href="" className="mx-6 bg-dark_pink w-16 h-16 rounded-[50%] hv-med">
           <img src="/public/Images/Frame 24.png " alt="" className="w-16 h-16"/>
         </a>
 
-        <a href="" className="mx-6 bg-dark_pink w-16 h-16 rounded-[50%]">
+        <a href="" className="mx-6 bg-dark_pink w-16 h-16 rounded-[50%] hv-med">
           <img src="/public/Images/ins.png" alt="" className="ml-4 mt-4 w-8 h-8"/>
         </a>
 
-        <a href="" className="mx-6 bg-dark_pink w-16 h-16 rounded-[50%]">
+        <a href="" className="mx-6 bg-dark_pink w-16 h-16 rounded-[50%] hv-med">
           <img src="/public/Images/tw.png" alt="" className="w-8 h-7 ml-4 mt-5"/>
         </a>
       </div>
